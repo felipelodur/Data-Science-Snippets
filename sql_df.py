0f5b942c2654f44e64483c9c50a43ab7a37f1260
Lodur <fcs4200@gmail.com>
2019-08-06 01:40:44 -0300

    SQL to DataFrame and DataFrame to SQL

---
+++ sql_df.py
@@ -0,0 +1,30 @@
+# ----- SQL to DataFrame
+
+host = 'example.rds.amazonaws.com'
+database = 'dbname'
+user = 'admin'
+password = 'password'
+port = 3306
+
+# Conectando
+con = mysql.connector.connect(user=user, 
+                              password=password,
+                              host=host,
+                              database=database)
+
+# Puxando as informações para um dataframe
+df = pd.read_sql_query("SELECT * from sales_data", con)
+
+# Fechando a conexão
+con.close()
+
+
+# ------ DataFrame to SQL
+
+from sqlalchemy import create_engine
+textEngine = 'mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(user,password,host,3306,database)
+engine = create_engine(textEngine, echo=False)
+
+df.to_sql('Produtos', con=engine, if_exists='append')
+con.close()
+
